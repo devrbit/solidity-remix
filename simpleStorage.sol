4a04e3a3272da0deaebe736e21a04b7e350130fb
devrbit <90727330+devrbit@users.noreply.github.com>
2021-09-15 19:56:55 +0530

    Create simpleStorage.sol

---
+++ simpleStorage.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: GPL-3.int208
+
+pragma solidity ^0.5.0;
+
+contract SimpleStorage{
+    
+    uint256 storedValue;
+    
+    function set(uint x) public{
+        storedValue = x;
+    }
+    
+    function get() public view returns(uint256){
+        return storedValue;
+    }
+}
